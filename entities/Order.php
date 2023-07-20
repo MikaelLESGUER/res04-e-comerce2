ceeb2d27df0df8b2184a562aaf387458eb858272
MikaëlLESGUER <mikedev35@outlook.fr>
2023-07-20 09:38:25 +0000

    Order.php

--- entities/Order.php
+++ entities/Order.php
@@ -6,40 +6,55 @@ class Order {
 	private ?int $id;
 	private int $user_id;
 	private string $order_date;
-	private string $address_id;
+	private string $amount;
+	private int $quantity;
 	
-	
-	public function __construct(string $user_id, string $order_date, int $address_id) {
+	public function __construct(string $user_id, string $order_date, string $amount, int $quantity) {
 		$this->user_id = $user_id;
 		$this->order_date = $order_date;
-		$this->address_id = $address_id;
+		$this->amount = $amount;
+		$this->quantity = $quantity;
 		$this->id = null;
 	}
+	
 	public function getId() : ?int {
 		return $this->id;
 	}
+	
 	public function getUserId() : int {
 		return $this->user_id;
 	}
+	
 	public function getOrderDate() : string {
 		return $this->order_date;
 	}
-	public function getAddressId() : string {
-		return $this->address_id;
+	
+	public function getAmount() : string {
+		return $this->amount;
+	}
+	
+	public function getQuantity() : int {
+		return $this->quantity;
 	}
 
 	public function setId(int $id) : void {
 		$this->id = $id;
 	}
+	
 	public function setUserId(int $user_id) : void {
 		$this->user_id = $user_id;
 	}
+	
 	public function setOrderDate(string $order_date) : void {
 		$this->order_date = $order_date;
 	}
-	public function setAddressId(int $address_id) : void {
-		$this->address_id = $address_id;
+	
+	public function setAmount(string $amount) {
+		$this->amount = $amount;
 	}
 	
+	public function setQuantity (int $quantity) {
+		$this->quantity = $quantity;
+	}
 }
 
