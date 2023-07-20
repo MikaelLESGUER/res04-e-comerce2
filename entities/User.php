1a8679d34b5a7a2ffc2089fb457db77ac6d5f10c
MikaëlLESGUER <mikedev35@outlook.fr>
2023-07-20 08:15:34 +0000

    modif

--- entities/User.php
+++ entities/User.php
@@ -4,12 +4,16 @@ class User
 {
     private ?int $id;
     private string $username;
+    private string $first_name;
+    private string $last_name;
     private string $email;
     private string $password;
     
     public function __construct(string $username, string $email, string $password)
     {
         $this->username = $username;
+        $this->first_name = $first_name;
+        $this->last_name = $last_name;
         $this->email = $email;
         $this->password = $password;
         $this->id = null;
@@ -24,10 +28,28 @@ class User
     {
         return $this->username;
     }
+    
     public function setUsername(string $username): void
     {
         $this->username = $username;
     }
+        
+    public function getFirstName(): string {
+        return $this->first_name;
+    }
+    
+    public function setFirstName(string $name): void {
+        $this->first_name = $name;
+    }
+    
+        public function setLastName(string $name): void {
+        $this->last_name = $name;
+    }
+    
+    public function getPassword(): string {
+        return $this->password;
+    }
+    
     public function getEmail(): string
     {
         return $this->email;
@@ -37,14 +59,17 @@ class User
     {
         $this->id = $id;
     }
+    
     public function setEmail(string $email): void
     {
         $this->email = $email;
     }
+    
     public function getPassword(): string
     {
         return $this->password;
     }
+    
     public function setPassword(string $password): void
     {
         $this->password = $password;
